bc16ed7a8bdceaed19e67d9fd7a0c893a4b6535a
obszank <obszank@gmail.com>
2024-12-31 08:38:45 +0100

    Added Chats and the top part of chat with user description

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,27 @@ import './chat.css'
 
 const Chat = () => {
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>NARUTOOOOOOOOOO KAWAZAKIIIIIIIIII</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+
+                </div>
+
+            </div>
+            <div className="center"></div>
+            <div className="bottom"></div>
+
+        </div>
     )
 }
 
